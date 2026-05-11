84826d582305debe74c0826c16c213df225b8cf5
流苏酥 <130482030+xxcchh-vv@users.noreply.github.com>
2026-05-11 21:53:34 +0800

    Add disk usage and retention cleanup

--- stream_sentinel/file_manager.py
+++ stream_sentinel/file_manager.py
@@ -1,3 +1,5 @@
+import shutil
+import time
 from pathlib import Path
 
 
@@ -11,14 +13,24 @@ class FileManager:
 
         for file in self.root.rglob('*'):
             if file.is_file():
+                stat = file.stat()
                 files.append(
                     {
                         'path': str(file),
-                        'size_mb': round(file.stat().st_size / 1024 / 1024, 2),
+                        'size_mb': round(stat.st_size / 1024 / 1024, 2),
+                        'modified_at': int(stat.st_mtime),
                     }
                 )
 
-        return sorted(files, key=lambda x: x['path'])
+        return sorted(files, key=lambda x: x['modified_at'], reverse=True)
+
+    def disk_usage(self):
+        usage = shutil.disk_usage(self.root)
+        return {
+            'total_gb': round(usage.total / 1024 / 1024 / 1024, 2),
+            'used_gb': round(usage.used / 1024 / 1024 / 1024, 2),
+            'free_gb': round(usage.free / 1024 / 1024 / 1024, 2),
+        }
 
     def cleanup_old_segments(self, suffix: str = '.ts'):
         removed = 0
@@ -29,5 +41,19 @@ class FileManager:
                 removed += 1
             except Exception:
                 pass
+
+        return removed
+
+    def cleanup_older_than_days(self, days: int):
+        cutoff = time.time() - days * 86400
+        removed = 0
+
+        for file in self.root.rglob('*'):
+            if file.is_file() and file.stat().st_mtime < cutoff:
+                try:
+                    file.unlink()
+                    removed += 1
+                except Exception:
+                    pass
 
         return removed
